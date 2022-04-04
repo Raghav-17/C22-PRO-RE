f8ef606336b64af842449d6afbbe8f4b080205fd
Raghav-17 <95995682+Raghav-17@users.noreply.github.com>
2022-04-04 14:45:22 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,58 +2,46 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
 
-var engine, world;
-var canvas;
-var palyer, playerBase, playerArcher;
-var baseimage;
+var canvas, angle, tower, ground, cannon;
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
 
+  canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  angleMode(DEGREES);
-
+  
   var options = {
     isStatic: true
-  };
+  }
 
-  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
-  World.add(world, playerBase);
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
 
-  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world,player)
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
 
- // playerArcher = new ( 340, playerBase.position.y - 112, 120, 120);
-    playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
- // playerArcher = PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
- // playerArcher = new PlayerArcher( );
-
+  angle = 20 
+  cannon = new Cannon(180,110,130,100,angle)
 }
 
 function draw() {
-  background(backgroundImg);
-  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-  image(playerimage,player.position.x,player.position.y,50,180)
-
+  image(backgroundImg,0,0,1200,600)
   Engine.update(engine);
+  
+  cannon.show()
+  
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  
 
-
-  // playerArcher.display;
-  // playerArcherdisplay();
-     playerArcher.display();
-  // display();
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
+  push();
+  imageMode(CENTER);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();  
 }
